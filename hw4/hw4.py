2e46cabccc82be027ef8933024c4de0bcb538e3e
aqeel13932 <samer226047@gmail.com>
2016-04-11 22:43:47 +0300

    hw4 finalized

---
+++ hw4/hw4.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from math import pow
+
+
+def showimg(img):
+    cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+    img = np.array(img,dtype=float)/float(255)
+    cv2.imshow('test',img)
+    cv2.resizeWindow('test',600,600)
+    cv2.waitKey(0)
+
+def readimg(name):
+    return cv2.imread(name,0)
+#Read The image
+mypic = readimg('mypicture.jpg')
+resized = cv2.resize(mypic,(512,512),interpolation=cv2.INTER_LANCZOS4)
+cv2.imwrite('mypicture_resized.jpg',resized )
+for i in range(4,10):
+    limit =  int(pow(2,i))
+    resized[0:limit,0:limit] = cv2.GaussianBlur(resized[0:limit,0:limit],(5,5),20)
+    #print limit
+cv2.imwrite('blurred_piece_by_piece.jpg',resized)
